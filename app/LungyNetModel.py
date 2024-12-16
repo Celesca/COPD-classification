7c19bf43bb7f76acbd76f53ea8b6686e465a3334
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-12-17 01:57:12 +0700

    add: LungyNetModel.py

--- app/LungyNetModel.py
+++ app/LungyNetModel.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 class LungyNetModel:
     def __init__(self):
         self.model = load_model('models/LungyNet.h5')
+        self.classes = ['Healthy', 'COPD']
 
     def preprocessing(self, audio_file, mode):
         sr_new = 16000
@@ -21,3 +22,10 @@ class LungyNetModel:
             feature = librosa.feature.melspectrogram(y=x, sr=sr_new, n_mels=128, fmax=8000)
             feature = librosa.power_to_db(feature, ref=np.max)
         return feature
+    
+    def predict(self, audio_file, mode='mfcc'):
+        preprocessed_audio = self.preprocessing(audio_file, mode)
+        preprocessed_audio = preprocessed_audio.reshape((1, 20, 157, 1))
+        prediction = self.model.predict(preprocessed_audio)
+        predicted_class = np.argmax(prediction, axis=1)
+        return self.classes[predicted_class[0]]
