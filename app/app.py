362f01a3a9ecd39770575f71a24bd5c0a6f01c95
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-12-17 01:58:03 +0700

    fix: app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from WheezeCrackleModel import WheezeCrackleModel
+from LungyNetModel import LungyNetModel
 
 app = Flask(__name__)
 
 WheezeCrackleModel = WheezeCrackleModel()
+LungyNetModel = LungyNetModel()
 
 @app.route("/")
 def hello_world():
